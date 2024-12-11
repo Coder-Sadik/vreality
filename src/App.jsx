e01ee6f8d860b4267d46425afa9c2c27b563fde9
Sadik Rahman <srahman2702@gmail.com>
2024-12-11 14:26:57 +0600

    nav menu created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navbar";
 
-export default function App() {
+function App() {
 	return (
 		<>
 			<Navbar />
 		</>
 	);
 }
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
-
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { navItems } from "../constants";
 const Navbar = () => {
-    return (
-        <div>
-            <h1>from navbar</h1>
-        </div>
-    );
+	const [menuOpen, setMenuOpen] = useState(false);
+	const toggleNavbar = () => setMenuOpen(!menuOpen);
+
+	return (
+		<nav className="sticky top-0 z-100 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+			<div className="container px-4 mx-auto relative text-sm">
+				<div className="flex justify-between items-center">
+
+					{/* icon and title */}
+					<div className="flex items-center flex-shrink-0">
+						<img className="h-10 w-10 mr-2" src={logo} alt="logo" />
+						<span className="text-xl tracking-tight">VReality</span>
+					</div>
+                    
+					{/* nav items */}
+					<ul className="hidden lg:flex ml-14 space-x-12">
+						{navItems.map((item, index) => (
+							<li key={index}>
+								<a href="{item.href}">{item.label}</a>
+							</li>
+						))}
+					</ul>
+
+					{/* buttons only for large screen */}
+					<div className="hidden lg:flex justify-center items-center space-x-12">
+						<a className="border rounded-md py-2 px-3" href="#">
+							SIgn In
+						</a>
+						<a
+							className=" rounded-md py-2 px-3 bg-gradient-to-r from-orange-500 to-orange-800"
+							href="#"
+						>
+							Create an account
+						</a>
+					</div>
+                    
+					{/* mobile menu */}
+					<div className="lg:hidden md:flex flex-col justify-end">
+						<button onClick={toggleNavbar}>
+							{menuOpen ? <X /> : <Menu />}
+						</button>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
